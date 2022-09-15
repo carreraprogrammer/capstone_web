50105aebd67a8d681fc59b080cebae8bf7349b4d
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-09-15 12:09:02 -0500

    fix java linter errors

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,14 +1,13 @@
-const burgerBtn = document.querySelector(".burger-menu");
-const navMobile = document.querySelector(".nav-navigation-mobile");
-const isActive = document.querySelectorAll(".is-active");
-const navLink = document.querySelectorAll(".nav-link");
+const burgerBtn = document.querySelector('.burger-menu');
+const navMobile = document.querySelector('.nav-navigation-mobile');
+const navLink = document.querySelectorAll('.nav-link');
 
-burgerBtn.addEventListener("click", () => {
-  navMobile.classList.toggle("is-active");
-  burgerBtn.classList.toggle("is-active");
+burgerBtn.addEventListener('click', () => {
+  navMobile.classList.toggle('is-active');
+  burgerBtn.classList.toggle('is-active');
 });
 
 navLink.forEach((link) => link.addEventListener('click', () => {
-  navMobile.classList.toggle("is-active");
-  burgerBtn.classList.toggle("is-active");
+  navMobile.classList.toggle('is-active');
+  burgerBtn.classList.toggle('is-active');
 }));
