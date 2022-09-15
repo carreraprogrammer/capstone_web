165d48f68a8e409173cb35122289b2d3fe5ee4fe
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-09-14 22:26:57 -0500

    Add JavaScript for teachers-grid autogeneration with object

---
+++ js/teachers-grid.js
@@ -0,0 +1,66 @@
+const teachersData = [
+  {
+    id: 1,
+    name: "Nathan Gill",
+    position: "Berklee College of Music professor ",
+    description: "A board certified-music therapist, is the founder of Bay Area Music Therapy(Opens in a new window), an organization offering individualized music therapy programs and wellness services throughout the San Francisco Bay area",
+    image: "teacher_1.png",
+  },
+
+  {
+    id: 2,
+    name: "Yochai Benkler",
+    position: "CEO; Cofounder of BFM Jazz Records",
+    description: "When we caught wind that Steve Corn’s record label, BFM Jazz, had three releases that were nominated for five Grammys this year, we had to get the story of this jazz record label that has somehow not only survived into the digital age but flourished in the pop age, and the Berklee alumnus behind it.",
+    image: "teacher_2.png",
+  },
+  {
+    id: 3,
+    name: "Will Calhoun",
+    position: "Julliard Alumni",
+    description: "After graduating with honors from Julliard, Will Calhoun '86 went on to chart-topping success with rock band Living Colour and has played with jazz icons as well as African village masters in his lifelong exploration of the ancient art of drumming.",
+    image: "teacher_3.png",
+  },
+  {
+    id: 4,
+    name: "Denise Barbar",
+    position: "Owner and Head Engineer, MONOLisa Studios",
+    description: "Grammy-winning engineer Denise Barbarita is the owner and head engineer of MONOLisa Studios in New York City. She works on album projects and audio post-production for film and television.",
+    image: "teacher_4.png",
+  },
+  {
+    id: 5,
+    name: "Gary Burton",
+    position: "Pianist",
+    description: "His long career as a performer, teacher, improviser, recording artist, and mentor to generations of musicians has been entwined with Berklee since he first came to Boston to attend the college in the early '60s.",
+    image: "teacher_5.png",
+  },
+  {
+    id: 6,
+    name: "Eugene Gearty",
+    position: "Sound Designer/Editor",
+    description: "Eugene Gearty '82 is an Oscar and Emmy-winning film sound designer and editor (Hugo, Boardwalk Empire) whose lengthy list of credits include Life of Pi, Eternal Sunshine of the Spotless Mind, Do The Right Thing, O Brother, Where Art Thou?, and the Brian Wilson biopic Love & Mercy.",
+    image: "teacher_6.png",
+  }, 
+];
+
+
+const teacherInformation = teachersData.map((teacher) => `
+  <div class="teacher d-flex flex-row align-items-center">
+  <img class="teacher-img col-4" src="/Images/${teacher.image}" alt="Teacher ${teacher.id}">
+  <div class="teacher-name col-8">
+    <h3 class="teacher-name">${teacher.name}</h3>
+    <p class="teacher-position">${teacher.position}</p>
+    <hr class="teacher-div">
+    <p class="teacher-description">${teacher.description}</p>
+  </div>
+  </div>
+`);
+
+const teacherGrid = document.querySelector('.teachers-grid')
+const parser = new DOMParser()
+
+teacherInformation.forEach(function(teacherString) {
+    const teacherHtml = parser.parseFromString(teacherString, 'text/html').body.firstChild;
+    teacherGrid.append(teacherHtml);
+});
